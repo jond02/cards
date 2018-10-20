1cf45f1226535ad4c39a358805947a77f9f7307c
Jonathan Dann <nwjon122@gmail.com>
2018-10-20 16:15:56 -0700

    for loop

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
-	fmt.Println(card)
+	cards := []string{"Ace", newCard()}
+	cards = append(cards, "Club")
+	fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
 }
 
 func newCard() string {
