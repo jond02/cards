2c7ccb0e29268984de86e75f97abb0463cd99453
Jonathan Dann <nwjon122@gmail.com>
2018-10-20 16:28:26 -0700

    deck type

--- main.go
+++ main.go
@@ -3,14 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	cards := []string{"Ace", newCard()}
+	cards := deck{"Ace", newCard()}
 	cards = append(cards, "Club")
+
 	fmt.Println(cards)
-
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
+	cards.print()
 }
 
 func newCard() string {
